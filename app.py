fe0dc33eb33ee03b6250d8e3c923e0af278b2646
Billiam Stewart Rodriguez <djcalanco@gmail.com>
2024-03-17 14:45:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-import streamlit as st
+from pathlib import Path
 
+import streamlit as st
+from PIL import Image
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+
+# --- LOAD CSS ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+    
 st.set_page_config(
     page_title="Virtual Barista",
     page_icon=":coffee:"
